cd0a58f3614c1c12fccb10ac2236f4b5b2411f6c
Arn0ldo <21670177@iguala.tecnm.mx>
2023-03-20 20:27:56 -0600

    primer commit

---
+++ Guitar.java
@@ -0,0 +1,11 @@
+public class Guitar {
+    private String SerialNumber;
+
+    private double price;
+    private int quantity;
+    private String model;;
+    private String color;
+    private String type;
+
+
+}
---
+++ Student.java
@@ -0,0 +1,24 @@
+public class Student {
+    private String name; //Jorge
+    private String lastname; //Perez
+    private int semester; //6
+    private int controlNumber; // 12345678
+    private String career; // English teacher
+    private double average; // 9.5
+
+
+    public String getName(String Name) {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+}
